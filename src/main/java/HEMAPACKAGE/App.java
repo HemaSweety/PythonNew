f8035421aa88f31e7a267badb09c3eb206f7072a
Hemalatha Nagella <hnagella@innominds.com>
2018-08-31 16:14:06 +0530

    maven commit

---
+++ src/main/java/HEMAPACKAGE/App.java
@@ -0,0 +1,13 @@
+package HEMAPACKAGE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
